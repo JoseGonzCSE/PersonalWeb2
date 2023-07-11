222db5749595740f4b82ef59817d8ee8bce56111
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2023-07-11 12:42:51 -0700

    Nav Bar: Pre active

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
+import Home from './Pages/Home';
+import AboutMe from './Pages/AboutMe';
 
 function App() {
-  return <Nav/>
+  let Page
+  switch(window.location.pathname){
+    case "/":
+      Page=Home
+      break
+    case "/aboutme":
+      Page=AboutMe
+      break
+  }
+  return (
+    <>
+      <Nav/>
+      <Page/>
+    </>
+  )
 }
 
 export default App;
