757621ccd658ee35caa4628071e06318125b78e8
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2023-07-11 12:23:00 -0700

    Created Buttons

---
+++ personal-web/src/Nav.js
@@ -0,0 +1,18 @@
+export default function Nav(){
+    return <nav className="Nav">
+        <a href ="/" className="Title">Main Page</a>
+        <ul>
+            <li>
+                <a href="/aboutme">About Me</a>
+            </li>
+            <li>
+                <a href="/LinkedIn">LinkedIn</a>
+            </li>
+            <li>
+                <a href="/GitHub">GitHub</a>
+            </li>
+
+            
+        </ul>
+    </nav>
+}
