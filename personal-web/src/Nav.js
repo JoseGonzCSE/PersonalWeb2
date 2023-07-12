ad10b8c7a8b3ca2e488652b92b909f27d5e622e3
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2023-07-12 11:59:26 -0700

    Pre-active
    Bug: using Children get's rid of text but links still work?

--- personal-web/src/Nav.js
+++ personal-web/src/Nav.js
@@ -1,20 +1,28 @@
+import { Children } from "react"
+
 export default function Nav(){
     return(
-     <nav className="Nav">
+    <nav className="Nav">
         <a href ="/" className="Title">Main Page</a>
         <ul>
-            <li className="active">
-                <a href="/aboutme">About Me</a>
-            </li>
+            <Link href="/aboutme">AboutMe</Link>
+            <Link href="/LinkedIn">LinkedIn</Link>
+            <Link href="/GitHub">GitHub</Link>
             <li>
-                <a href="/LinkedIn">LinkedIn</a>
+                <a href="/test">Test</a>
             </li>
-            <li>
-                <a href="/GitHub">GitHub</a>
-            </li>
-
-            
         </ul>
     </nav>
+    )
+}
+
+// When Children, stops working?
+function Link({href,...props}){
+    const path= window.location.pathname
+
+    return(
+        <li className={path===href ? "active": ""}>
+            <a href={href} {...props}></a>
+        </li>
     )
 }
