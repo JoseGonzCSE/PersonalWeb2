99e30c04c06d45d1bfa1d654f1cfbe6de96c4491
JoseGonzCSE <jgonzalez437@ucmerced.edu>
2023-07-18 20:29:49 -0700

    Left side Home Prototype
    Added my name, brief description, simple css, skills, image folder

--- personal-web/src/Pages/Home.js
+++ personal-web/src/Pages/Home.js
@@ -1,4 +1,16 @@
 import './Home.css'
+import csslogo from './Imgs/css-file.png'
+import cLogo from './Imgs/development.png'
+import djangoLogo from './Imgs/django.png'
+import htmlLogo from './Imgs/html.png'
+import javascriptLogo from './Imgs/javascript.png'
+import reactLogo from './Imgs/react.png'
+import pythonLogo from './Imgs/python.png'
+
+
+
+
+
 export default function Home(){
     return (
         <div className='infoBox'>
@@ -6,9 +18,19 @@ export default function Home(){
                 <div className='pageLeftAll'>
                     <h2 className="main">Nice to meet you, my name is</h2>
                     <h1 className="name"> Jose 'Andres' Gonzalez</h1>
-                </div>
-                <div>            
-                    <p className="subText"> This is the subText</p>
+                    <p className="subText">Recent graduate in Computer Science and Engineering located in San Francisco </p>
+                    <div className='skillContainer'>
+                        <p className="skills">Skills |</p>
+                        <div className="logos">
+                            <img src={pythonLogo} alt="pythonLogo"/>
+                            <img src={djangoLogo} alt="DjangoLogo"/>
+                            <img src={cLogo} alt="cLogo"/>
+                            <img src={htmlLogo} alt="htmlLogo"/>
+                            <img src={csslogo} alt="csslogo"/>
+                            <img src={javascriptLogo} alt="javascriptLogo"/>
+                            <img src={reactLogo} alt="reactLogo"/>
+                        </div>
+                    </div>
                 </div>
             </div>
 
